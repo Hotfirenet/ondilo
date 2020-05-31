06a9166f74c95c3b0d8e67659727081571771dac
Johan <hotfirenet@gmail.com>
2020-05-31 21:33:45 +0200

    add type battery

--- core/class/ondilo.class.php
+++ core/class/ondilo.class.php
@@ -138,6 +138,7 @@ class ondilo extends eqLogic {
                     $eqLogic->setConfiguration( 'typeDisinfection', $pool['disinfection']['primary'] );
                     $eqLogic->setConfiguration( 'uvSanitizer'     , $pool['disinfection']['secondary']['uv_sanitizer'] );
                     $eqLogic->setConfiguration( 'ozonator'        , $pool['disinfection']['secondary']['ozonator'] );
+                    $eqLogic->getConfiguration( 'battery_type'    , 'Lithium');
                 
                     $deviceInfos = $ondilo->getDevice( $pool['id'] );
                     
